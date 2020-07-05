e18f756e04ca06667204683222651937fba8f2c0
Tsgzj <wxsun1991@gmail.com>
2020-07-05 16:34:09 -0700

    Modified keymap for leetcode mode

--- modules/app/leetcode/config.el
+++ modules/app/leetcode/config.el
@@ -4,7 +4,7 @@
 (map! :leader
       (:prefix-map ("a L" . "Leetcode")
        :desc "Leetcode"             "l" #'leetcode
-       :desc "Show current problem" "d" #'leetcode-show-current-problem
+       :desc "Show current problem" "s" #'leetcode-show-current-problem
        :desc "Refresh"              "r" #'leetcode-refresh
        :desc "Try the solution"     "t" #'leetcode-try
        :desc "Submit the solution"  "u" #'leetcode-submit))
