cef185b6d103224a7021e121b71c25daada9f620
Tsgzj <swx@tsgzj.me>
2022-05-16 15:27:32 -0700

    Using ivy

--- init.el
+++ init.el
@@ -39,8 +39,8 @@
        ;;minimap           ; show a map of the code on the side
        ;; modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash           ; blink cursor line after big motions
-       neotree           ; a project drawer, like NERDTree for vim
-       ophints           ;highlight the region an operation acts on
+       ;; neotree           ; a project drawer, like NERDTree for vim
+       ophints             ;highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ligatures       ; ligatures or substitute text with pretty symbols
        ;; tabs              ; an tab bar for Emacs
@@ -144,7 +144,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +present)        ; organize your plain life in plain text
+       (org +roam2 +present)        ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
